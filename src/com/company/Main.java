a628cee3c99296da53d3e9d87c6c9d10ca9221f2
fspadea <fs279@pvsd.org>
2016-12-16 14:29:42 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int [] [] ticTacToe = new int[3][3];
+        System.out.println("       |       |\n_______|_______|_______\n       |       |\n_______|_______|_______\n       |       |\n       |       |\n");
+    }
+}
