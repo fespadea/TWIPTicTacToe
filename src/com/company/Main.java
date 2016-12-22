a01443ff726ac976803d191ff877f680a5819b9e
fspadea <fs279@pvsd.org>
2016-12-22 14:26:41 -0500

    progress and stuff

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Moves.printBoard();
-        Moves.enterCoordinate();
+        System.out.println("Player 1 is X. Player 2 is O. In person vs computer, person is player 1.");
+        Moves.pvp();
     }
 }
--- src/com/company/Moves.java
+++ src/com/company/Moves.java
@@ -8,30 +8,62 @@ import java.util.Scanner;
 public class Moves {
     public static String [] [] position = {{" ", " ", " "}, {" ", " ", " "}, {" ", " ", " "}};
     public static Scanner input = new Scanner(System.in);
-    public static boolean [] [] ticTacToe = new boolean[3][3];
     public static boolean player = true;
-    public static boolean [] [] playerRecord = new boolean[3][3];
+    public static boolean continueGame = true;
+    public static String winner = "";
     static void printBoard(){
-        System.out.println("    " + position[2][0] +"   |    " + position[2][1] + "   |   " + position[2][2] + "\n________|________|________\n    " + position[1][0] + "   |    " + position[1][1] + "   |   " + position[1][2] + "\n________|________|________\n    " + position[0][0] + "   |    " + position[0][1] + "   |   " + position[0][3] + "\n        |        |\n");
+        System.out.println("     |     |\n  " + position[2][0] +"  |  " + position[2][1] + "  |  " + position[2][2] + "\n_____|_____|_____\n   " + "  |     |\n  " + position[1][0] + "  |  " + position[1][1] + "  |  " + position[1][2] + "\n_____|_____|_____\n" + "     |     |\n  " + position[0][0] + "  |  " + position[0][1] + "  |  " + position[0][2] + "\n     |     |");
     }
     static void enterCoordinate(){
         System.out.println("Gimme a coordinate on the board. (bottom left being the origin) Template: (x,y)");
         String coor = input.next();
         setter(Integer.parseInt(coor.substring(1,2)), Integer.parseInt(coor.substring(3,4)));
         player = !player;
-        playerRecord [Integer.parseInt(coor.substring(1,2))] [Integer.parseInt(coor.substring(3,4))] = player;
     }
-    static void setter(int a, int b){
-        for(boolean [] i: ticTacToe){
-            int pi = 0;
-            for(boolean j: i){
-                pi++;
-                if(i[pi] == true){
-                    if(player == true){
-                        position[]
-                    }
-                }
-            }
+    static void setter(int a, int b) {
+        if (position[b][a].equals(" ")) {
+            if (player == true) {
+                position[b][a] = "X";
+            } else
+                position[b][a] = "O";
+        }
+        else{
+            System.out.println("That's taken. Choose something else.");
+            enterCoordinate();
+        }
+    }
+    static void pvp(){
+        repeaterForPvp();
+        repeaterForPvp();
+        repeaterForPvp();
+        repeaterForPvp();
+        repeaterForPvp();
+        repeaterForPvp();
+        repeaterForPvp();
+        repeaterForPvp();
+        repeaterForPvp();
+        if(continueGame){
+            System.out.print("It's a tie.");
+        }
+    }
+    static void repeaterForPvp(){
+        checkForWin();
+        if(continueGame){
+            printBoard();
+            enterCoordinate();
+        }
+    }
+    public static void checkForWin(){
+        if((position[0][0].equals("X") && position[0][1].equals("X") && position[0][2].equals("X")) || (position[1][0].equals("X") && position[1][1].equals("X") && position[1][2].equals("X")) || (position[2][0].equals("X") && position[2][1].equals("X") && position[2][2].equals("X")) || (position[0][0].equals("X") && position[1][0].equals("X") && position[2][0].equals("X")) || (position[0][1].equals("X") && position[1][1].equals("X") && position[2][1].equals("X")) || (position[0][2].equals("X") && position[1][2].equals("X") && position[2][2].equals("X")) || (position[0][0].equals("X") && position[1][1].equals("X") && position[2][2].equals("X")) || (position[2][0].equals("X") && position[1][1].equals("X") && position[2][0].equals("X"))){
+            continueGame = false;
+            winner = "Player 1";
+        }
+        if((position[0][0].equals("O") && position[0][1].equals("O") && position[0][2].equals("O")) || (position[1][0].equals("O") && position[1][1].equals("O") && position[1][2].equals("O")) || (position[2][0].equals("O") && position[2][1].equals("O") && position[2][2].equals("O")) || (position[0][0].equals("O") && position[1][0].equals("O") && position[2][0].equals("O")) || (position[0][1].equals("O") && position[1][1].equals("O") && position[2][1].equals("O")) || (position[0][2].equals("O") && position[1][2].equals("O") && position[2][2].equals("O")) || (position[0][0].equals("O") && position[1][1].equals("O") && position[2][2].equals("O")) || (position[2][0].equals("O") && position[1][1].equals("O") && position[2][0].equals("O"))){
+            continueGame = false;
+            winner = "Player 2";
+        }
+        if(!continueGame){
+            System.out.print("The winner is " + winner);
         }
     }
 }
