13472b881bb11c754fd8b9b2ac42e640652fe314
Fernando Spadea <fespadearocks@gmail.com>
2016-12-25 21:10:26 -0500

    computer progress

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Player 1 is X. Player 2 is O. In person vs computer, person is player 1.");
-        Moves.pvp();
+        System.out.println("PVP=0   PVC=1   CVC=2");
+        Scanner mode = new Scanner(System.in);
+        int gameMode = mode.nextInt();
+        if(gameMode == 0) {
+            Moves.pvp();
+        }
+        else if(gameMode == 1){
+            Moves.pvc();
+        }
     }
 }
--- src/com/company/Moves.java
+++ src/com/company/Moves.java
@@ -1,6 +1,6 @@
 package com.company;
 
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by fs279 on 12/19/16.
@@ -32,38 +32,138 @@ public class Moves {
             enterCoordinate();
         }
     }
-    static void pvp(){
-        repeaterForPvp();
-        repeaterForPvp();
-        repeaterForPvp();
-        repeaterForPvp();
-        repeaterForPvp();
-        repeaterForPvp();
-        repeaterForPvp();
-        repeaterForPvp();
-        repeaterForPvp();
-        if(continueGame){
-            System.out.print("It's a tie.");
+    static void pvc(){
+        for(int x = 0; x < 4; x++){
+            if(continueGame){
+                printBoard();
+                enterCoordinate();
+                checkForWin();
+            }
+            if(continueGame){
+                printBoard();
+                computerPick();
+                checkForWin();
+            }
         }
-    }
-    static void repeaterForPvp(){
         if(continueGame){
             printBoard();
             enterCoordinate();
             checkForWin();
         }
+        if(continueGame){
+            System.out.print("It's a tie.");
+        }
+    }
+    static void computerPick(){
+        player = !player;
+        if (position[1][1].equals(" ")) {
+            position[1][1] = "O";
+        }
+        else{
+            int o = 0;
+            for(int c = 0; c < 3; c++) {
+                int y = 0;
+                int [] v = new int[2];
+                for (int x = 0; x < 3; x++) {
+                    int [] n = new int[2];
+                    if(position[c][x].equals("X")){
+                        n[y] = x;
+                        y++;
+                    }
+                    if(y == 2){
+                        v = n;
+                    }
+                }
+                if(y == 2) {
+                    int j = 3;
+                    for (int i : v) {
+                        j -= i;
+                    }
+                    if (position[j][c].equals(" ")) {
+                        position[j][c] = "O";
+                        o = 2;
+                        break;
+                    }
+                }
+            }
+            int l = 0;
+            if(o != 2) {
+                int s = 0;
+                int m [] = new int[2];
+                for (int x = 0; x < 3; x++) {
+                    if (position[x][x] == "X") {
+                        m[s] = x;
+                        s++;
+                    }
+                    if(s == 2) {
+                        int j = 3;
+                        for (int i : m) {
+                            j -= i;
+                        }
+                        if (position[j][j].equals(" ")) {
+                            position[j][j] = "O";
+                            l = 2;
+                            break;
+                        }
+                    }
+                }
+            }
+            if(l != 2) {
+                int s = 0;
+                int m [] = new int[2];
+                for (int x = 0; x < 3; x++) {
+                    if (position[2-x][x] == "X") {
+                        m[s] = x;
+                        s++;
+                    }
+                    if(s == 2) {
+                        int j = 3;
+                        for (int i : m) {
+                            j -= i;
+                        }
+                        if (position[2-j][j].equals(" ")) {
+                            position[2-j][j] = "O";
+                            l = 2;
+                            break;
+                        }
+                    }
+                }
+            }
+            for(int x = 0; x < 3; x++){
+                for(int y = 0; y < 3; y++){
+                    if(position[x][y].equals(" ")){
+                        position[x][y] = "O";
+                        x = 3;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    static void pvp(){
+        for(int x = 0; x < 9; x++){
+            if(continueGame){
+                printBoard();
+                enterCoordinate();
+                checkForWin();
+            }
+        }
+        if(continueGame){
+            System.out.print("It's a tie.");
+        }
     }
     public static void checkForWin(){
-        if((position[0][0].equals("X") && position[0][1].equals("X") && position[0][2].equals("X")) || (position[1][0].equals("X") && position[1][1].equals("X") && position[1][2].equals("X")) || (position[2][0].equals("X") && position[2][1].equals("X") && position[2][2].equals("X")) || (position[0][0].equals("X") && position[1][0].equals("X") && position[2][0].equals("X")) || (position[0][1].equals("X") && position[1][1].equals("X") && position[2][1].equals("X")) || (position[0][2].equals("X") && position[1][2].equals("X") && position[2][2].equals("X")) || (position[0][0].equals("X") && position[1][1].equals("X") && position[2][2].equals("X")) || (position[2][0].equals("X") && position[1][1].equals("X") && position[2][0].equals("X"))){
+        if((position[0][0].equals("X") && position[0][1].equals("X") && position[0][2].equals("X")) || (position[1][0].equals("X") && position[1][1].equals("X") && position[1][2].equals("X")) || (position[2][0].equals("X") && position[2][1].equals("X") && position[2][2].equals("X")) || (position[0][0].equals("X") && position[1][0].equals("X") && position[2][0].equals("X")) || (position[0][1].equals("X") && position[1][1].equals("X") && position[2][1].equals("X")) || (position[0][2].equals("X") && position[1][2].equals("X") && position[2][2].equals("X")) || (position[0][0].equals("X") && position[1][1].equals("X") && position[2][2].equals("X")) || (position[0][2].equals("X") && position[1][1].equals("X") && position[2][0].equals("X"))){
             continueGame = false;
             winner = "Player 1";
         }
-        if((position[0][0].equals("O") && position[0][1].equals("O") && position[0][2].equals("O")) || (position[1][0].equals("O") && position[1][1].equals("O") && position[1][2].equals("O")) || (position[2][0].equals("O") && position[2][1].equals("O") && position[2][2].equals("O")) || (position[0][0].equals("O") && position[1][0].equals("O") && position[2][0].equals("O")) || (position[0][1].equals("O") && position[1][1].equals("O") && position[2][1].equals("O")) || (position[0][2].equals("O") && position[1][2].equals("O") && position[2][2].equals("O")) || (position[0][0].equals("O") && position[1][1].equals("O") && position[2][2].equals("O")) || (position[2][0].equals("O") && position[1][1].equals("O") && position[2][0].equals("O"))){
+        if((position[0][0].equals("O") && position[0][1].equals("O") && position[0][2].equals("O")) || (position[1][0].equals("O") && position[1][1].equals("O") && position[1][2].equals("O")) || (position[2][0].equals("O") && position[2][1].equals("O") && position[2][2].equals("O")) || (position[0][0].equals("O") && position[1][0].equals("O") && position[2][0].equals("O")) || (position[0][1].equals("O") && position[1][1].equals("O") && position[2][1].equals("O")) || (position[0][2].equals("O") && position[1][2].equals("O") && position[2][2].equals("O")) || (position[0][0].equals("O") && position[1][1].equals("O") && position[2][2].equals("O")) || (position[0][2].equals("O") && position[1][1].equals("O") && position[2][0].equals("O"))){
             continueGame = false;
             winner = "Player 2";
         }
         if(!continueGame){
-            System.out.print("The winner is " + winner);
+            System.out.println("The winner is " + winner);
+            printBoard();
         }
     }
 }
